ab54d14ace0325c17bc4acd01cf2d1ed6f673e43
Ose Akhigbe <oseboboakhigbe@gmail.com>
2017-11-21 15:09:55 +0000

    Initial Commit

---
+++ F&H App/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  F&H App
+//
+//  Created by Osereime Akhigbe on 21/11/2017.
+//  Copyright © 2017 University of Leeds. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ F&H App/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  F&H App
+//
+//  Created by Osereime Akhigbe on 21/11/2017.
+//  Copyright © 2017 University of Leeds. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
